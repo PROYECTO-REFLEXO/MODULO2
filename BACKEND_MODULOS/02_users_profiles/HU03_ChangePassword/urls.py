bc9d8bd56bd73649eb3b5d1a2dc687b4f4a84385
BandoWed <bazan@escuela.it>
2025-08-15 10:32:29 -0500

    Descripción clara de los cambios

--- BACKEND_MODULOS/02_users_profiles/HU03_ChangePassword/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from controllers.auth.ChangePasswordController import ChangePasswordController
-
-urlpatterns = [
-    path('change-password/', ChangePasswordController.as_view()),
-    path('validate-password/', ChangePasswordController.as_view(), {'http_method': 'validate_password'}),
-]
