ba74497396fc8f832c6e3b7e0d2c950bbd6201c3
BandoWed <bazan@escuela.it>
2025-08-19 12:00:10 -0500

    guardar

--- 02_users_profiles/manage.py
+++ 02_users_profiles/manage.py
@@ -3,13 +3,10 @@
 import os
 import sys
 
-# Agregar el directorio actual al path de Python
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
